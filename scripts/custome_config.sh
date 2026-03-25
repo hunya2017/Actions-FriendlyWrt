58cecb76d1a1a492a92684f7f15e3b8fbafdfff1
hunya2017 <hunya2017@gmail.com>
2026-03-25 09:55:39 +0800

    6

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -4,12 +4,8 @@ sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
 
-# 修改 LAN IP为 192.168.8.1，子网掩码255.255.252.0，广播地址192.168.11.255
-sed -i 's/option ipaddr '\''192\.168\.[0-9]*\.[0-9]*'\''/option ipaddr '\''192.168.8.1'\''/g' friendlywrt/target/linux/rockchip/files/etc/config/network
-sed -i 's/option netmask '\''255\.255\.[0-9]*\.[0-9]*'\''/option netmask '\''255.255.252.0'\''/g' friendlywrt/target/linux/rockchip/files/etc/config/network
-sed -i '/option ipaddr/a\    option broadcast '\''192.168.11.255'\''' friendlywrt/target/linux/rockchip/files/etc/config/network
-
-# 添加 99-default-settings 初始化脚本
+# 添加99-default-settings初始化脚本
+# LAN IP配置在此脚本中设置
 mkdir -p friendlywrt/package/base-files/files/etc/uci-defaults
 cat > friendlywrt/package/base-files/files/etc/uci-defaults/99-default-settings <<'EOF'
 #!/bin/sh
@@ -19,12 +15,10 @@ cat > friendlywrt/package/base-files/files/etc/uci-defaults/99-default-settings 
 # log potential errors
 exec >/tmp/setup.log 2>&1
 
-# Configure LAN with custom IP
+# Configure LAN with custom IP (CIDR notation: /22 = 255.255.252.0)
 lan_ip_address="192.168.8.1/22"
 if [ -n "$lan_ip_address" ]; then
   uci set network.lan.ipaddr="$lan_ip_address"
-  uci set network.lan.netmask="255.255.252.0"
-  uci set network.lan.broadcast="192.168.11.255"
   uci commit network
 fi
 
